0d112dcba37faa1f2807bfdd3d1bc6c8d783923b
Nileemagaykwad <nileema.gaykwad1994@gmail.com>
2018-08-09 16:36:24 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { SafePipe } from './safe.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SafePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
